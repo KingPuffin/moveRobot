5c15ab0410f7033ec4eda66c4a060a36a534247a
Kevan Mordan <kevanmordan@gmail.com>
2013-11-21 14:49:58 -0700

    Library for moving the robot

---
+++ move.h
@@ -0,0 +1,14 @@
+
+#ifndef MOVE_H_
+#define MOVE_H_
+
+void initTimer();
+void moveForward();
+void moveBackward();
+void shortRight();
+void longRight();
+void shortLeft();
+void longLeft();
+void fullStop();
+
+#endif
